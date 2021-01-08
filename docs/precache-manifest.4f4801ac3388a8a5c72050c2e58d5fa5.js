06480601a83f6913c58df9df54f0102365ee27ff
BlockchainBear <64762751+BlockchainBear@users.noreply.github.com>
2021-01-08 08:24:43 -0800

    new changes for git pages, plus new build

---
+++ docs/precache-manifest.4f4801ac3388a8a5c72050c2e58d5fa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d6372a552dca2a8779abd8eec86ff59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57de4c69017cbbf347da",
+    "url": "/static/css/main.42a420bb.chunk.css"
+  },
+  {
+    "revision": "a3189dd90bd7726e1ec5",
+    "url": "/static/js/2.6549ce60.chunk.js"
+  },
+  {
+    "revision": "57de4c69017cbbf347da",
+    "url": "/static/js/main.3afc34b5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
