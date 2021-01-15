56e20e3930e4cbcc167cf4d19534d50789f4e15e
BlockchainBear <64762751+BlockchainBear@users.noreply.github.com>
2021-01-14 23:29:40 -0800

    error handling

--- src/components/app/form/Form.js
+++ src/components/app/form/Form.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import Feature from './Feature'
-
+import Features from './Features'
 
 
 
 class Form extends React.Component {
     render () {
-        const features = Object.keys(FEATURES).map((feature, idx) => {
+        const features = Object.keys(Features).map((feature, idx) => {
             const featureHash = feature + '-' + idx;
             return (
                 <Feature
                     key={featureHash}
                     featureHash={featureHash}
                     feature={feature}
-                    features={FEATURES}
+                    features={Features}
                     selected={this.props.selected}
                     USCurrencyFormat={this.props.USCurrencyFormat}
                     handleItemChange={this.props.handleItemChange}
